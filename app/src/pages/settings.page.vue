70d399552779b16305c402070e4c236a24cb04ea
Michael Oesterle <michaeloesterle01@gmail.com>
2024-09-20 14:30:46 +0200

    Improve account actions

--- app/src/pages/settings.page.vue
+++ app/src/pages/settings.page.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page>
-    <q-list class="q-pt-md">
+    <q-list class="q-pa-sm q-pt-md">
       <q-item-label header>User</q-item-label>
       <q-item class="q-pb-lg">
           <q-item-section avatar>
@@ -28,15 +28,14 @@
 
       <q-item-label header>Accounts</q-item-label>
 
-      <q-item v-for="account in accounts" :key="account.id" class="q-pb-lg" :class="{ 'bg-grey-2': $store.account.id == account.id }">
+      <q-item v-for="account in accounts" :key="account.id" :class="{ 'bg-grey-2': $store.account.id == account.id }">
           <q-item-section>
               <q-item-label>{{ account.description }} <q-icon v-if="$store.user?.defaultAccount == account.id" name="verified_user" color="positive" /></q-item-label>
               <q-item-label caption lines="1">{{ account.id }}</q-item-label>
           </q-item-section>
 
           <q-item-section side>
-              <q-btn v-if="$store.account.id != account.id" flat round icon="login" color="primary" @click="onClickSwitchAccounts(account.id)" />
-              <q-btn v-else flat round icon="more_horiz" color="grey" @click="onClickAccountActions(account.id)" />
+              <q-btn flat round icon="more_horiz" color="grey" @click="onClickAccountActions(account)" />
           </q-item-section>
       </q-item>
 
@@ -54,8 +53,9 @@
         </q-item-section>
       </q-item>
     </q-list>
-    {{ $store.user }}
   </q-page>
+
+  <account-actions-dialog v-model="accountActionsDialog.show" :account="accountActionsDialog.account" @unset-default="unsetDefaultAccount" @login="onClickSwitchAccounts" />
 </template>
 
 <script setup>
@@ -65,8 +65,15 @@ import { onMounted, ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useStore } from 'src/stores/store';
 
+import AccountActionsDialog from 'src/components/settings/accountActions.dialog.vue';
+
 const $router = useRouter();
 const $store = useStore();
+
+const accountActionsDialog = ref({
+  show: false,
+  account: null
+});
 
 const api = ref('');
 const apiStatus = ref(false);
@@ -95,8 +102,13 @@ const onClickUserActions = () => {
 
 };
 
-const onClickAccountActions = () => {
+const onClickAccountActions = (account) => {
+  accountActionsDialog.value.account = account;
+  accountActionsDialog.value.show = true;
+};
 
+const unsetDefaultAccount = async () => {
+  console.log('unsetDefaultAccount');
 };
 
 const onClickSignout = async () => {
